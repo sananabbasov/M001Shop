4cf11fb5155861e1e13163003b7220c8dbe815c7
sananabbasov <sanan_abbasov@list.ru>
2022-07-14 17:17:09 +0400

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductManager
+    {
+        public void Add(Product product)
+        {
+           
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        { 
+            // Sql
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
